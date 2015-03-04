feranick/3dpv

commit 8b8b1527e0e0b0775ce836424524375fbcada973

Fix strings not terminate in main.cpp

3DPV_software_NO-MC/src/main.cpp
@@ -601,6 +601,7 @@ void ReadBestObjFile ()  // takes pointer to first element in new coordinate arr
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,bestFile);
+	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -788,6 +789,7 @@ int ReadStructureVertices_Count (int model_ind)
 
   // copy the file into the buffer:
   result = fread (buffer,1,lSize,pFile);
+	*buffer++ = '\0';
 //  if (result != lSize) {fputs ("Reading error",stderr); exit (3);} // check gives integer comparison warning
 
   // the whole file is now loaded in the memory buffer.
@@ -837,6 +839,7 @@ void ReadStructureVertices_Retrieve (double *coordinates, int model_ind)  // tak
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,pFile);
+	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -886,6 +889,7 @@ void ReadMainOutputFile (char *output_mainfile)  // takes pointer to first eleme
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,pFile);
+	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -963,6 +967,7 @@ void ReadRoofPositions(double in_pos[], int nextline_num, int i_am_best)
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,posFile);
+	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.

commit b48d692ad3cb67f173fe3aa8d015f6310f46dfec

Fix regression in adding termination to string in main.cpp
introduced in 8b8b1527e0e0b0775ce836424524375fbcada973

3DPV_software_NO-MC/src/main.cpp
@@ -601,7 +601,6 @@ void ReadBestObjFile ()  // takes pointer to first element in new coordinate arr
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,bestFile);
-	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -789,7 +788,6 @@ int ReadStructureVertices_Count (int model_ind)
 
   // copy the file into the buffer:
   result = fread (buffer,1,lSize,pFile);
-	*buffer++ = '\0';
 //  if (result != lSize) {fputs ("Reading error",stderr); exit (3);} // check gives integer comparison warning
 
   // the whole file is now loaded in the memory buffer.
@@ -839,7 +837,6 @@ void ReadStructureVertices_Retrieve (double *coordinates, int model_ind)  // tak
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,pFile);
-	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -889,7 +886,6 @@ void ReadMainOutputFile (char *output_mainfile)  // takes pointer to first eleme
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,pFile);
-	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.
@@ -967,7 +963,6 @@ void ReadRoofPositions(double in_pos[], int nextline_num, int i_am_best)
 
 	// copy the file into the buffer:
 	result = fread (buffer,1,lSize,posFile);
-	*buffer++ = '\0';
 //	if (result != lSize) {fputs ("Reading error",stderr); exit (3);}   // check gives integer comparison warning
 
 	// the whole file is now loaded in the memory buffer.